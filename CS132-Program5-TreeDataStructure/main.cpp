5fa02c58149dc1c0a276e17af6ef22b7791fc42a
Taylor Bisset <taylorbisset@gmail.com>
2023-11-07 19:06:29 -0800

    Assignment description updated

--- CS132-Program5-TreeDataStructure/main.cpp
+++ CS132-Program5-TreeDataStructure/main.cpp
@@ -3,9 +3,9 @@
  Section:       A
  Program Name:  CS132-Program5-TreeDataStructure
 
- Description:   Manages sorted doubly linked lists of strings.
-                Reads data from input files, manipulates lists,
-                and writes results to output files.
+ Description:   This program is a simple and incomplete version 
+                of the Binary Search Tree data structure that only
+                includes 3 functions for the class. 
 
 Repo:           https://github.com/TaylorBisset/CS132-Program5-TreeDataStructure
 */
